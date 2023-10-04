ac0a6d819abd450311b33603a70b401a80137f5f
NicolasPozzer <pozzernico@gmail.com>
2023-10-04 16:35:27 -0300

    publish in git!

---
+++ src/main/java/com/ejercicio1/estudianteprogramacion/EstudianteProgramacionApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicio1.estudianteprogramacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudianteProgramacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudianteProgramacionApplication.class, args);
+	}
+
+}
